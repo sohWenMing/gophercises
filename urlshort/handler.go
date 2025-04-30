e58fafcfee95a24042072484b820eb5da58523d2
wen ming soh <wenming.soh@temus.com>
2025-04-30 10:21:16 +0800

    finished exercise

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -1,6 +1,7 @@
 package urlshort
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"strings"
@@ -17,7 +18,7 @@ import (
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	//	TODO: Implement this...
 	return func(w http.ResponseWriter, r *http.Request) {
-		foundUrl, isFound := searchUrls(pathsToUrls, r)
+		foundUrl, isFound := mapHandlerSearchURLS(pathsToUrls, r)
 		switch isFound {
 		case true:
 			http.Redirect(w, r, foundUrl, http.StatusSeeOther)
@@ -26,12 +27,11 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 		}
 	}
 }
-func searchUrls(pathsToUrls map[string]string, r *http.Request) (foundUrl string, isFound bool) {
+
+func mapHandlerSearchURLS(pathsToUrls map[string]string, r *http.Request) (foundUrl string, isFound bool) {
 	isFound = false
 	foundUrl = ""
-	url := r.URL
-	path := url.Path
-	pathStrings := strings.Split(path, "/")
+	pathStrings := getPathsFromRequest(r)
 	for _, pathString := range pathStrings {
 		formedString := fmt.Sprintf("/%s", pathString)
 		if _, ok := pathsToUrls[formedString]; ok {
@@ -45,6 +45,79 @@ func searchUrls(pathsToUrls map[string]string, r *http.Request) (foundUrl string
 	default:
 		return
 	}
+}
+
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	parsed, err := ParseYAML(yml)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(parsed) == 0 {
+		return nil, errors.New("yaml returned no information")
+	}
+	for _, parsed := range parsed {
+		fmt.Println(parsed.repr())
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		foundUrl, isFound := yamlHandlerSearchUrls(parsed, r)
+		switch isFound {
+		case true:
+			http.Redirect(w, r, foundUrl, http.StatusSeeOther)
+			return
+		default:
+			fallback.ServeHTTP(w, r)
+			return
+		}
+	}, nil
+}
+
+func yamlHandlerSearchUrls(pathToUrls []*PathToUrl, r *http.Request) (foundUrl string, isFound bool) {
+	isFound = false
+	foundUrl = ""
+	pathStrings := getPathsFromRequest(r)
+	for _, pathString := range pathStrings {
+		fmt.Println("evaluating path: ", pathString)
+		formedPath := fmt.Sprintf("/%s", pathString)
+		fmt.Println("current formedPath: ", formedPath)
+		for _, pathToUrl := range pathToUrls {
+			fmt.Println("current pathToURlPath", pathToUrl.Path)
+			if formedPath == pathToUrl.Path {
+				foundUrl = pathToUrl.URL
+				isFound = true
+				return
+			}
+		}
+	}
+	return
+}
+
+func ParseYAML(data []byte) (parsed []*PathToUrl, err error) {
+	parsedData := []PathToUrl{}
+	err = yaml.Unmarshal(data, &parsedData)
+	if err != nil {
+		return nil, err
+	}
+	for _, record := range parsedData {
+		parsed = append(parsed, &record)
+	}
+	return parsed, nil
+}
+
+type PathToUrl struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
+
+func (p *PathToUrl) repr() string {
+	return fmt.Sprintf("Path: %s\nURL %s", p.Path, p.URL)
+}
+func getPathsFromRequest(r *http.Request) []string {
+	url := r.URL
+	path := url.Path
+	pathStrings := strings.Split(path, "/")
+	return pathStrings
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -63,21 +136,3 @@ func searchUrls(pathsToUrls map[string]string, r *http.Request) (foundUrl string
 //
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
-func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
-}
-
-func ParseYAML(data []byte) (parsed []*PathToUrl, err error) {
-	parsedData := []PathToUrl{}
-	err = yaml.Unmarshal(data, &parsedData)
-	if err != nil {
-		return nil, err
-	}
-	return parsed, nil
-}
-
-type PathToUrl struct {
-	Path string `yaml:"path"`
-	URL  string `yaml:"url"`
-}
